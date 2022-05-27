81ed9020f7bbb85023f100fa4b3d88716da30519
Shihao Cao <shehowcow@gmail.com>
2022-05-27 01:19:25 -0400

    pre renderer error hack

--- components/landing-elements.js
+++ components/landing-elements.js
@@ -1,7 +1,8 @@
 // import Head from 'next/head'
 import Link from 'next/link'
 
-export function notes_list_element(title, sub_folder){
+export function notes_list_element(input_title, sub_folder){
+    let title = input_title + "" // hack to fix next js prerenderer error
     let js_file_name = title.replaceAll(' ', '-')
     js_file_name = js_file_name.toLowerCase()
     let full_path = `/${sub_folder}/${js_file_name}`
@@ -12,7 +13,8 @@ export function notes_list_element(title, sub_folder){
     </li>
   }
 
-export function notes_list_element_full(title, file_name, sub_folder){
+export function notes_list_element_full(input_title, file_name, sub_folder){
+    let title = input_title + "" // hack to fix next js prerenderer error
     let full_path = `/${sub_folder}/${file_name}`
     return <li>
         <Link href={full_path} >
