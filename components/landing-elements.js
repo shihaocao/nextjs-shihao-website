f7473afd5cf664e169aff48e2b24d4c16c49d97b
Shihao Cao <shehowcow@gmail.com>
2022-05-27 01:20:08 -0400

    try with const

--- components/landing-elements.js
+++ components/landing-elements.js
@@ -2,7 +2,7 @@
 import Link from 'next/link'
 
 export function notes_list_element(input_title, sub_folder){
-    let title = input_title + "" // hack to fix next js prerenderer error
+    const title = input_title + "" // hack to fix next js prerenderer error
     let js_file_name = title.replaceAll(' ', '-')
     js_file_name = js_file_name.toLowerCase()
     let full_path = `/${sub_folder}/${js_file_name}`
@@ -14,7 +14,7 @@ export function notes_list_element(input_title, sub_folder){
   }
 
 export function notes_list_element_full(input_title, file_name, sub_folder){
-    let title = input_title + "" // hack to fix next js prerenderer error
+    const title = input_title + "" // hack to fix next js prerenderer error
     let full_path = `/${sub_folder}/${file_name}`
     return <li>
         <Link href={full_path} >
