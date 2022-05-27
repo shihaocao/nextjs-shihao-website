c9ff596670ef2f433a287e2b53ec62b21aff6b77
Shihao Cao <shehowcow@gmail.com>
2022-05-27 01:24:34 -0400

    split join instead of replace all

--- components/landing-elements.js
+++ components/landing-elements.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export function notes_list_element(input_title, sub_folder){
     const title = input_title + "" // hack to fix next js prerenderer error
-    let js_file_name = title.replaceAll(' ', '-')
+    let js_file_name = title.split(' ').join('-')
     js_file_name = js_file_name.toLowerCase()
     let full_path = `/${sub_folder}/${js_file_name}`
     return <li>
